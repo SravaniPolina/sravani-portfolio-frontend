f7eb5f72dd195b33e03d8901b52e94e0cfa6fc66
SravaniPolina <sravanip1911@gmail.com>
2025-11-25 22:11:26 +0530

    Fix import and usage of NavBar and StatsCards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 // src/App.js
 import './index.css';
 import React from "react";
-import Navbar from './components/NavBar';
+import NavBar from './components/NavBar';      
 import Hero from "./components/Hero";
+import StatsCards from "./components/StatsCards";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from './components/Footer';
-import StatsCards from "./components/StatsCards"; // at the top with other imports
-
-// ... inside your App render/return...
-<Hero />
-<StatsCards />
 
 function App() {
   return (
     <div className="antialiased text-slate-800">
-      <Navbar />
+      <NavBar />
       <main>
         <Hero />
+        <StatsCards />
         <Projects />
         <Contact />
       </main>
