0d62b46197d16a8997460e9cc48817e6950efe53
SravaniPolina <sravanip1911@gmail.com>
2026-06-26 13:22:34 +0530

    Update ProfessionalJourney.js

--- src/components/ProfessionalJourney.js
+++ src/components/ProfessionalJourney.js
@@ -6,7 +6,7 @@ export default function ProfessionalJourney() {
       <div className="max-w-6xl mx-auto">
         <div className="flex justify-center">
           <img
-            src="/experience-roadmap.jpg"
+            src="/Experience_Roadmap.jpg"
             alt="Career roadmap: Relationship Manager at ICICI and IDBI Bank 2012-2017, Associate Director at TiE Amaravati 2018-2019, Head of Strategy at Srinivasa Farms 2019-2023, Commercial Strategy Manager at Noveltech Feeds 2022-2023, Chief of Staff at Mahathi Infra Services 2024-2025, Independent Advisor 2025-present"
             className="w-full max-w-6xl rounded-2xl shadow-lg"
           />
