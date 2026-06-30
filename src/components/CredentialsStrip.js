d009af932ff1cb766b812ced4912f5169882b8bc
SravaniPolina <sravanip1911@gmail.com>
2026-06-30 14:12:05 +0530

    Update CredentialsStrip.js

--- src/components/CredentialsStrip.js
+++ src/components/CredentialsStrip.js
@@ -5,7 +5,7 @@ export default function CredentialsStrip() {
     <div className="bg-white py-2 px-4">
       <div className="flex justify-center">
         <img
-          src="/credentials-strip.jpg"
+          src="/Credentials_Strip.jpg"
           alt="Credentials: IIM Lucknow, IIM Kozhikode, GenAI Mastermind, IDBI Bank, ICICI Bank, TiE Amaravati"
           className="w-full max-w-4xl"
         />
