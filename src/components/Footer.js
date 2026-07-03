0d97a351bee543df779c8c3345f48ea8fc5624e7
SravaniPolina <sravanip1911@gmail.com>
2026-07-03 16:21:06 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#080808] border-t border-white/10">
+    <footer className="border-t border-white/10" style={{background: "var(--surface)"}}>
 
       <div className="max-w-7xl mx-auto px-6 py-16">
 
