c88914d57f6c8b0baf8c7302bc2ccd1d6cf8c48e
SravaniPolina <sravanip1911@gmail.com>
2026-07-03 15:02:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import StatStrip from "./components/StatStrip";
 import AboutMe from "./components/AboutMe";
 import StrategicLeadershipServices from "./components/StrategicLeadershipServices";
 import Projects from "./components/Projects";
-import LeadershipPhilosophy from "./components/MyAdvisoryApproach";
+import LeadershipPhilosophy from "./components/LeadershipPhilosophy";
 import ExecutiveTestimonials from "./components/ExecutiveTestimonials";
 import ProfessionalJourney from "./components/ProfessionalJourney";
 import DirectContact from "./components/DirectContact";
