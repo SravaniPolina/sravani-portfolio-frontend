52d2066f5e548112e9c1d72e598dcc99e9cf4eb1
SravaniPolina <sravanip1911@gmail.com>
2026-07-13 10:38:43 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import AboutMe from "../components/AboutMe";
-import LeadershipPhilosophy from "../components/LeadershipPhilosophy";
+import AboutMe from "./components/AboutMe";
+import LeadershipPhilosophy from "./components/LeadershipPhilosophy";
 
 export default function About() {
   return (
