8c40626e852bd29bb79aa2f0820ede7ce00adc5e
SravaniPolina <sravanip1911@gmail.com>
2026-07-13 10:37:54 +0530

    Update Services.jsx

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import StrategicLeadershipServices from "../components/StrategicLeadershipServices";
-import Projects from "../components/Projects";
+import Projects from "./components/Projects";
 
 export default function Services() {
   return (
